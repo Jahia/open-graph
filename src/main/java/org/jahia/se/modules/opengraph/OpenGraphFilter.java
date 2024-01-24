7a051e938ae72e064b020e2e26dd5cc097de5fdd
hduchesne <hduchesne@jahia.com>
2024-01-24 19:12:19 +0100

    bug fixes

--- src/main/java/org/jahia/se/modules/opengraph/OpenGraphFilter.java
+++ src/main/java/org/jahia/se/modules/opengraph/OpenGraphFilter.java
@@ -12,6 +12,8 @@ import org.jahia.services.render.filter.RenderFilter;
 import org.jetbrains.annotations.NotNull;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.jcr.RepositoryException;
 import java.io.IOException;
@@ -19,7 +21,11 @@ import java.util.List;
 
 @Component(service = RenderFilter.class)
 public class OpenGraphFilter  extends AbstractFilter {
+    public static final Logger logger = LoggerFactory.getLogger(OpenGraphFilter.class);
+
     private final static String OPENGRAPH_MODULE="open-graph";
+    private final static String OPENGRAPH_MIXIN="jmix:openGraph";
+
     @Activate
     public void activate() {
         setPriority(0);// -1 launch after addStuff
@@ -33,11 +39,12 @@ public class OpenGraphFilter  extends AbstractFilter {
     public String execute(String previousOut, RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {
         String output = super.execute(previousOut, renderContext, resource, chain);
         boolean isInstalled = isInstalledModule(renderContext,OPENGRAPH_MODULE);
-
+        JCRNodeWrapper currentNode = renderContext.getMainResource().getNode();
+        boolean isEnabled =  currentNode.isNodeType(OPENGRAPH_MIXIN);
         //Disable the filter in case we are in Content Editor preview.
         boolean isCEPreview = renderContext.getRequest().getAttribute("ce_preview") != null;
 
-        if(isInstalled && !isCEPreview){
+        if(isInstalled && isEnabled && !isCEPreview){
 
             //update output to add scripts
             output = enhanceOutput(output, renderContext);
@@ -64,7 +71,8 @@ public class OpenGraphFilter  extends AbstractFilter {
 //            final StartTag headStartTag = elementList.get(0).getStartTag();
 //            outputDocument.insert(headStartTag.getEnd(),getHeadScript(renderContext));
             final EndTag headEndTag = elementList.get(0).getEndTag();
-            outputDocument.insert(headEndTag.getBegin()-1,getHeadScript(renderContext));
+            String ogScript = getHeadScript(renderContext);
+            outputDocument.insert(headEndTag.getBegin()-1,ogScript);
         }
 
         output = outputDocument.toString().trim();
@@ -80,26 +88,32 @@ public class OpenGraphFilter  extends AbstractFilter {
         String ogTitle = currentNode.getPropertyAsString("j:ogTitle");
         String ogDescription = currentNode.getPropertyAsString("j:ogDescription");
         String ogType = currentNode.getPropertyAsString("j:ogType");
-        JCRNodeWrapper ogImageNode = (JCRNodeWrapper) currentNode.getProperty("j:ogImage").getNode();
 
+        StringBuilder headScriptBuilder = new StringBuilder("\n<meta property=\"og:site_name\" content=\""+siteName+"\" />");
+        if(ogTitle!=null && !ogTitle.isEmpty())
+            headScriptBuilder.append("\n<meta property=\"og:title\" content=\""+ogTitle+"\" />");
+        if(ogDescription!=null && !ogDescription.isEmpty())
+            headScriptBuilder.append("\n<meta property=\"og:description\" content=\""+ogDescription+"\" />");
+        if(ogType!=null && !ogType.isEmpty())
+            headScriptBuilder.append("\n<meta property=\"og:type\" content=\""+ogType+"\" />");
 
-        StringBuilder headScriptBuilder =
-                new StringBuilder("\n<meta property=\"og:title\" content=\""+ogTitle+"\" />");
-        headScriptBuilder.append("\n<meta property=\"og:description\" content=\""+ogDescription+"\" />");
-        headScriptBuilder.append("\n<meta property=\"og:type\" content=\""+ogType+"\" />");
-        if(ogImageNode != null){
-            String width = ogImageNode.getPropertyAsString("j:width");
-            String height = ogImageNode.getPropertyAsString("j:height");
-            String alt = ogImageNode.getDisplayableName();
-            headScriptBuilder.append("\n<meta property=\"og:image\" content=\""+hostname+ogImageNode.getUrl()+"\" />");
-            headScriptBuilder.append("\n<meta property=\"og:image:width\" content=\""+width+"\" />");
-            headScriptBuilder.append("\n<meta property=\"og:image:height\" content=\""+height+"\" />");
-            headScriptBuilder.append("\n<meta property=\"og:image:alt\" content=\""+alt+"\" />");
-        }
         headScriptBuilder.append("\n<meta property=\"og:locale\" content=\""+language+"\" />");
-        headScriptBuilder.append("\n<meta property=\"og:site_name\" content=\""+siteName+"\" />");
         headScriptBuilder.append("\n<meta property=\"og:url\" content=\""+hostname+currentNode.getUrl()+"\" />");
 
+        try {
+            JCRNodeWrapper ogImageNode = (JCRNodeWrapper) currentNode.getProperty("j:ogImage").getNode();
+            if(ogImageNode != null){
+                String width = ogImageNode.getPropertyAsString("j:width");
+                String height = ogImageNode.getPropertyAsString("j:height");
+                String alt = ogImageNode.getDisplayableName();
+                headScriptBuilder.append("\n<meta property=\"og:image\" content=\""+hostname+ogImageNode.getUrl()+"\" />");
+                headScriptBuilder.append("\n<meta property=\"og:image:width\" content=\""+width+"\" />");
+                headScriptBuilder.append("\n<meta property=\"og:image:height\" content=\""+height+"\" />");
+                headScriptBuilder.append("\n<meta property=\"og:image:alt\" content=\""+alt+"\" />");
+            }
+        }catch (Exception e){
+            logger.info("no image selected in opengraph for content : "+currentNode.getDisplayableName());
+        }
 
         return headScriptBuilder.toString();
     }
@@ -120,7 +134,7 @@ public class OpenGraphFilter  extends AbstractFilter {
         String host = renderContext.getRequest().getServerName();
         int port = renderContext.getRequest().getServerPort();
         String hostname = host;
-        if(port!= 80){
+        if(port!= 80 && port!= 443){
             hostname = host + ":" + String.valueOf(port);
         }
         return hostname;
